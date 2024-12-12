9aa162f9b3d41a64415417b4df5cb0d9b2a53fab
kirrtann <sonagrakirtan@gmail.com>
2024-12-12 18:58:45 +0530

    Done Search without Css

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,12 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 interface Movie {
+    Search:string;
     Title: string;
     Year: number;
     imdbID: string;
     Poster: string;
+
 }
 
 const API = "https://www.omdbapi.com/?apikey=19f4e811&";
@@ -46,10 +48,11 @@ const Home = () => {
     return (
         <>
             <div className="bg-gray-800 min-h-screen">
-                <div className="text-center text-white py-10">
-                    <h1 className="text-4xl font-serif mb-4">Search The Movies By Name And See the Details page</h1>
+                <div className="flex justify-between">
+                <div className="text-center text-white pt-5 ">
+                    <h1 className="text-4xl pl-5 font-serif mb-4">Search The Movies </h1>
                 </div>
-                <div className="flex justify-center py-10">
+                <div className=" pr-5 py-5">
                     <form className="w-full max-w-md" onSubmit={SearchMovies}>
                         <div className="relative">
                             <input
@@ -58,39 +61,43 @@ const Home = () => {
                                 value={searchValue}
                                 name="search"
                                 type="search"
-                                className="w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-700 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400"
+                                
+                                className="w-full p-4 pl-5 text-sm  outline-none text-white border-gray-300 rounded-lg bg-gray-700 focus:ring-blue-500 focus:border-blue-500 "
                             />
                             <button
                                 type="submit"
-                                className="absolute right-2.5 top-2.5 bg-blue-600 text-white font-medium rounded-lg text-sm px-4 py-2 hover:bg-blue-700"
+                                className="absolute right-2.5 top-2.5 bg-blue-600 text-white  rounded-lg text-sm px-4 py-2 hover:bg-blue-700"
                             >
                                 Search
                             </button>
                         </div>
                     </form>
                 </div>
+                </div>
                 <div className="container mx-auto px-4">
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
                         {Mydata.length > 0 ? (
                             Mydata.map((post) => (
-                                <Link to={"Moviesdetail/" + post.imdbID} key={post.imdbID}>
-                                <div className="bg-gray-700 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-all">
-                                    <img
-                                        className="w-full h-[350px] object-cover rounded-xl mb-4"
-                                        src={post.Poster}
-                                        alt={post.Title}
-                                    />
-                                    <h2 className="text-xl text-white truncate">{post.Title}</h2>
-                                    <p className="text-sm text-red-400">{post.Year}</p>
-                                </div>
-                            </Link>
-                            
+                                <Link to={"Moviesdetail/" + post.imdbID  } key={post.imdbID}>
+                                    <div className="bg-gray-700 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-all">
+                                        <img
+                                            className="w-full h-[350px] object-cover rounded-xl mb-4"
+                                            src={post.Poster}
+                                            alt={post.Title}
+                                        />
+                                        <h2 className="text-xl text-white truncate">{post.Title}</h2>
+                                        <p className="text-sm text-red-400">{post.Year}</p>
+                                    </div>
+                                </Link>
                             ))
                         ) : (
-                            <h1 className="text-3xl text-white font-serif">Search a Movie</h1>
+                            <div className="col-span-2 sm:col-span-3 md:col-span-4 flex mx-auto h-[300px]">
+                                <h1 className="text-3xl text-white font-serif">Search a Movie</h1>
+                            </div>
                         )}
                     </div>
                 </div>
+
             </div>
         </>
     );
--- src/Moviesdetail.tsx
+++ src/Moviesdetail.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 interface MovieDetail {
@@ -17,7 +17,9 @@ const API = "https://www.omdbapi.com/?apikey=19f4e811&";
 const Moviesdetail = () => {
   const { imdbID } = useParams();
   const [movie, setMovie] = useState<MovieDetail | null>(null);
+  const navigate = useNavigate(); 
 
+ 
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
@@ -32,30 +34,43 @@ const Moviesdetail = () => {
       fetchMovieDetails();
     }
   }, [imdbID]);
+  const BackButton = () => {
+    navigate(-1); 
+  };
 
   return (
-   <div className="bg-gray-800 min-h-screen text-white py-10 font-serif">
-  {movie ? (
-    <div className="max-w-4xl mx-auto  px-4">
-      <img
-        src={movie.Poster}
-        alt={movie.Title}
-        className=" object-cover rounded-lg mb-6 shadow-lg transform transition-transform "
-      />
-      <h1 className="text-4xl font-serif font-semibold mb-4 text-gray-100">{movie.Title}</h1>
-      <p className="text-lg text-gray-300 mb-4">{movie.Plot}</p>
-      <div className="mt-4 space-y-2">
-        <p className="text-xl">Year: <span className="font-semibold">{movie.Year}</span></p>
-        <p className="text-xl">Director: <span className="font-semibold">{movie.Director}</span></p>
-        <p className="text-xl">Genre: <span className="font-semibold">{movie.Genre}</span></p>
+    <>
+      <div className="bg-gray-800">
+        <button
+          className="text-white ml-5 px-3 mt-2 rounded-md border"
+          onClick={BackButton} 
+        >
+          Back
+        </button>
       </div>
-    </div>
-  ) : (
-    <h1 className="text-center text-3xl text-gray-400">Loading movie details...</h1>
-  )}
-</div>
-
+      <div className="bg-gray-800 min-h-screen text-white py-10 font-serif">
+        {movie ? (
+          <div className="max-w-4xl mx-auto px-4">
+            <img
+              src={movie.Poster}
+              alt={movie.Title}
+              className="object-cover rounded-lg mb-6 shadow-lg transform transition-transform"
+            />
+            <h1 className="text-4xl font-serif font-semibold mb-4 text-gray-100">{movie.Title}</h1>
+            <p className="text-lg text-gray-300 mb-4">{movie.Plot}</p>
+            <div className="mt-4 space-y-2">
+              <p className="text-xl">Year: <span className="font-semibold">{movie.Year}</span></p>
+              <p className="text-xl">Director: <span className="font-semibold">{movie.Director}</span></p>
+              <p className="text-xl">Genre: <span className="font-semibold">{movie.Genre}</span></p>
+            </div>
+          </div>
+        ) : (
+          <h1 className="text-center text-3xl text-gray-400">Loading movie details...</h1>
+        )}
+      </div>
+    </>
   );
 };
 
 export default Moviesdetail;
+
