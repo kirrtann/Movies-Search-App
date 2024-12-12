9a7283fa3bd90e01f8b809510bd299da22ee6604
kirrtann <sonagrakirtan@gmail.com>
2024-12-12 17:11:54 +0530

    contect file comment

--- src/context.tsx
+++ src/context.tsx
@@ -1,34 +1,34 @@
-import React, { ReactNode, useContext, useEffect } from "react";
-const AppContext = React.createContext("");
-const API_URl = `https://www.omdbapi.com/?apikey=19f4e811&s=titanic`
-interface AppProviderProps {
-  children: ReactNode;
-}
+// import React, { ReactNode, useContext, useEffect } from "react";
+// const AppContext = React.createContext("");
+// const API_URl = `https://www.omdbapi.com/?apikey=19f4e811&s=titanic`
+// interface AppProviderProps {
+//   children: ReactNode;
+// }
 
-const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
-  const getMovies = async (url:string) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      // console.log(data);
-      // console.log(res);
+// const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+//   const getMovies = async (url:string) => {
+//     try {
+//       const res = await fetch(url);
+//       const data = await res.json();
+//       // console.log(data);
+//       // console.log(res);
 
-    } 
-    catch (error) {
-      console.log(error);
+//     } 
+//     catch (error) {
+//       console.log(error);
 
-    }
-  }
-  useEffect(() => {
-    getMovies(API_URl)
-  }, [])
+//     }
+//   }
+//   useEffect(() => {
+//     getMovies(API_URl)
+//   }, [])
 
-  return <AppContext.Provider value={""}>{children}</AppContext.Provider>;
-};
+//   return <AppContext.Provider value={""}>{children}</AppContext.Provider>;
+// };
 
 
-const UseGlobalCOntext = () => {
-  return useContext(AppContext)
+// const UseGlobalCOntext = () => {
+//   return useContext(AppContext)
 
-}
-export { AppContext, AppProvider, UseGlobalCOntext };
+// }
+// export { AppContext, AppProvider, UseGlobalCOntext };
